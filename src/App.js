44fcc59eb678cd1b8e245766cf0e5a9cd229873e
debimsantos <debimortola@gmail.com>
2018-10-25 21:18:26 -0700

    Adding script and map coordinates

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.loadMap()
+  }
+
+  // Display map
+  loadMap = () => {
+    addScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCtbzZIfTIawx8Cdnrf0HxTJsQZ9767k-I&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  // Initialize map with Wespark lat lng
+  initMap = () => {
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: 33.687533, lng: -117.816489 },
+      zoom: 13
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <div id='map'></div>
+      </main>
     );
   }
+}
+
+// Script for the google map
+let addScript = (src) => {
+  let index = window.document.getElementsByTagName('script')[0]
+  let script = window.document.createElement('script')
+  script.src = src
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App;
