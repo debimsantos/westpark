667ec273df8b280ce724fda9449a75cd92b72448
debimsantos <debimortola@gmail.com>
2018-10-27 15:58:57 -0700

    Foursquare data added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 //import { getVenues } from './FourSquareAPI.js';
 import Map from './components/Map.js';
+import SquareAPI from './FourSquareAPI';
 
 class App extends Component {
   constructor() {
@@ -13,6 +14,14 @@ class App extends Component {
       zoom: 12
     };
   }
+
+    componentDidMount() {
+      SquareAPI.search({
+        ll: "33.689826, -117.806625",
+        query: "trail",
+        limit: 10
+      }).then(results => console.log(results))
+    }
 
   render() {
     return (
--- src/FourSquareAPI.js
+++ src/FourSquareAPI.js
@@ -1,25 +1,63 @@
-import axios from 'axios';
+/*
+* Adding FourSquare location information
+* Resource: Forrest Walker's Walkthrough: https://www.youtube.com/watch?v=Dj5hzKBxCBI&list=PL4rQq4MQP1crXuPtruu_eijgOUUXhcUCP&index=3
+*/
 
 
-// Get venues from FourSquare
-export const getVenues = (resp, err) => {
-    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
-    const parameters = {
-        client_id: "TALUJVAXB14U3KB0FQWWR45U3LHOHYAXBOOOCCYMYLQWUESN",
-        client_secret: "XNQRJOX32Q1LR25S1XLDFM2DHKACOAN5RYVTB3HFNP3PAAGF",
-        query: "trail",
-        ll: "33.687533,-117.816489",
-        radius: 8000,
-        limit: 10,
-        v: "20181019"
+class Helper {
+  static baseURL() {
+    // base API URL for Foursquare
+    return "https://api.foursquare.com/v2";
+  }
+  static auth() {
+    // auth credentials
+    const keys = {
+      client_id: "TALUJVAXB14U3KB0FQWWR45U3LHOHYAXBOOOCCYMYLQWUESN",
+      client_secret: "XNQRJOX32Q1LR25S1XLDFM2DHKACOAN5RYVTB3HFNP3PAAGF",
+      v: "20181027"
+    };
+    return Object.keys(keys)
+      .map(key => `${key}=${keys[key]}`)
+      .join("&");
+  }
+  static urlBuilder(urlPrams) {
+    if (!urlPrams) {
+      return ""
     }
-
-    axios.get(endPoint + new URLSearchParams(parameters))
-        .then(res => {
-            resp({ data: res.data.response.groups[0].items });
-        })
-        .catch(error => {
-            //console.log("Error fetching venues from FourSquare:" + error)
-            err({ error: error })
-        })
+    return Object.keys(urlPrams)
+      .map(key => `${key}=${urlPrams[key]}`)
+      .join("&");
+  }
+  // Request headers
+  static headers() {
+    return {
+      Accept: "application/json"
+    };
+  }
+  // Fetch Request
+  static simpleFetch(endPoint, method, urlPrams) {
+    let requestData = {
+      method,
+      headers: Helper.headers()
+    };
+    return fetch(`${Helper.baseURL()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(
+      urlPrams
+    )}`,
+      requestData
+    ).then(res => res.json()).catch(() => alert('Oops! looks like something went wrong finding locations'))
+  }
+}
+export default class SquareAPI {
+  // Get venues
+  static search(urlPrams) {
+    return Helper.simpleFetch("/venues/search", "GET", urlPrams);
+  }
+  // Get details of that venue
+  static getVenueDetails(VENUE_ID) {
+    return Helper.simpleFetch(`/venues/${VENUE_ID}`, "GET")
+  }
+  // Get images
+  static getVenuePhotos(VENUE_ID) {
+    return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
+  }
 }
